76aafa582475610756b3d687d9ae8221152eecdf
Sean Gilbertson <sean.gilbertson@gmail.com>
2020-04-12 02:01:22 +0000

    Init main page

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+
+
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <title>Pokedecks</title>
+      </Head>
+      <h1>Heading</h1>
+    </>
+  )
+}
